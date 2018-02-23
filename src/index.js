a45413ffc1c43a197c437bdf1530123dacb23ed1
ssaegert <ssaegert@gmail.com>
2018-02-23 14:57:59 -0600

    added form and submit button with functionality to post

--- src/index.js
+++ src/index.js
@@ -15,12 +15,47 @@ getMovies().then((movies) => {
   $("span").html(" ");
   movies.map(({title, rating, id}) => {
     // console.log(`id#${id} - ${title} - rating: ${rating}`);
-      $("span").append(`<dl>`);
-    $("span").append(`<dt>${title}</dt><dd>rating: ${rating}</dd>`);
-      $("span").append(`</dl>`);
+      $("#movie-list").append(`<dl>`);
+    $("#movie-list").append(`<dt>${title}</dt><dd>rating: ${rating}</dd>`);
+      $("#movie-list").append(`</dl>`);
 
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+
+//form -------------------
+
+
+$("#movie-form").submit(function(e) {
+    e.preventDefault();
+    // $("#city-weather").html("");
+    // $("#current-forecast").html("");
+    // latLon.newLat = $("#latitude").val();
+    // latLon.newLon = $("#longitude").val();
+    // forecast(latLon);
+    // $("#weather-form")[0].reset();
+    console.log($("#movie-title").val());
+    console.log($("#movie-rating").val());
+const movieList = {title: $("#movie-title").val(), rating: $("#movie-rating").val()};
+const url = '/api/movies';
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movieList),
+};
+fetch(url, options)
+    .then(console.log('post was created successfully'))
+    .catch(console.log('error'));
+});
+
+// const movieTile;
+// const movieRating;
+//
+//
+//
